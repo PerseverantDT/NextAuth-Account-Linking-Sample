894da402602eabd032193a2647b41f452c7fd194
Perseverant Determination <perseverant-determination@outlook.com>
2025-04-12 23:02:00 +0800

    Create MongoDB adapter for NextAuth.js

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth, { NextAuthConfig } from 'next-auth';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import { connectMongoClient } from '@/lib/db';
 
 const config: NextAuthConfig = (() => {
     return  {
+        adapter: MongoDBAdapter(connectMongoClient),
         providers: []
     }
 })();
