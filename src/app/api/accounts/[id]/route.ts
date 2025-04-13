f3678934e9afea68b20a72dae46051d11428960b
Perseverant Determination <perseverant-determination@outlook.com>
2025-04-13 08:16:40 +0800

    Filter account details returned by API

--- src/app/api/accounts/[id]/route.ts
+++ src/app/api/accounts/[id]/route.ts
@@ -1,5 +1,6 @@
 import { connectMongoClient } from "@/lib/db";
 import { ObjectId } from "mongodb";
+import { Account } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{id: string}> }) : Promise<NextResponse>
@@ -7,7 +8,13 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{id: s
     const { id } = await params;
 
     const database = connectMongoClient().db();
-    const accounts = await database.collection('accounts').find({ userId: new ObjectId(id) }).toArray();
+    const accounts = await database.collection('accounts').find<Account>({ userId: new ObjectId(id) }).toArray();
 
-    return NextResponse.json(accounts);
+    return NextResponse.json(accounts.map((account) => {
+        return {
+            userId: account.userId,
+            provider: account.provider,
+            accountId: account.providerAccountId
+        };
+    }));
 }
